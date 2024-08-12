77b185c98d6bcbe03907f73fa049a8f57da095ec
mohit7feb <45337317+mohit7feb@users.noreply.github.com>
2024-08-12 18:28:06 +0530

    Update about.php

--- about.php
+++ about.php
@@ -125,7 +125,7 @@
             </div>
             <div class="social_icon">
                <ul>
-                  <li>
+<!--                   <li>
                      <a ><i class="fa fa-facebook" aria-hidden="true"></i></a>
                   </li>
                   <li>
@@ -136,7 +136,7 @@
                   </li>
                   <li>
                      <a><i class="fa fa-instagram" aria-hidden="true"></i></a>
-                  </li>
+                  </li> -->
                </ul>
             </div>
          </div>
