84e00b7fbec7fe8e1a7277a6c2fde7a7eb33bf49
mohit7feb <45337317+mohit7feb@users.noreply.github.com>
2024-08-12 18:28:42 +0530

    Update client.php

--- client.php
+++ client.php
@@ -165,7 +165,7 @@
             </div>
             <div class="social_icon">
                <ul>
-                  <li>
+<!--                   <li>
                      <a ><i class="fa fa-facebook" aria-hidden="true"></i></a>
                   </li>
                   <li>
@@ -176,7 +176,7 @@
                   </li>
                   <li>
                      <a><i class="fa fa-instagram" aria-hidden="true"></i></a>
-                  </li>
+                  </li> -->
                </ul>
             </div>
          </div>
